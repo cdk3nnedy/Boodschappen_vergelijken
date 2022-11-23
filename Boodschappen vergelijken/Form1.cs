e6117ecb494cf856a027001fb5f5b3c4fd44dc7d
Your Name <you@example.com>
2022-11-23 21:05:50 +0100

    my sql reference geinstallerd

--- Boodschappen vergelijken/Form1.cs
+++ Boodschappen vergelijken/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Boodschappen_vergelijken
 {
@@ -29,7 +30,13 @@ namespace Boodschappen_vergelijken
 
         private void button1_Click(object sender, EventArgs e)
         {
-           ;
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open  !");
+            cnn.Close();
         }
     }
 }
